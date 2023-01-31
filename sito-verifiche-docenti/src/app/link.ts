929b806f48936eb551a1bc67c6389bcc2a8d77b7
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2023-01-31 10:43:58 +0000

    funziona

--- sito-verifiche-docenti/src/app/link.ts
+++ sito-verifiche-docenti/src/app/link.ts
@@ -1,4 +1,4 @@
 export class flaskLink {
     // Inserire il link e tenere lo slash alla fine
-    public static _API='https://5000-saccullop-verifichedoce-7g1balxeafz.ws-eu84.gitpod.io/';
+    public static _API='https://5000-saccullop-verifichedoce-wsb613watvi.ws-eu84.gitpod.io/';
 }
